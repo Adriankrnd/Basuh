93a30141f6b57c94b1a24a619feea32fd7fbe00f
Millennian Ibnu Adriansyah Karinda <millenniankarinda@gmail.com>
2023-05-20 21:27:14 +0700

    Initial Commit

---
+++ Basuh Watch App/BasuhApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasuhApp.swift
+//  Basuh Watch App
+//
+//  Created by Millennian Ibnu Adriansyah Karinda on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Basuh_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basuh Watch AppUITests/Basuh_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Basuh_Watch_AppUITestsLaunchTests.swift
+//  Basuh Watch AppUITests
+//
+//  Created by Millennian Ibnu Adriansyah Karinda on 20/05/23.
+//
+
+import XCTest
+
+final class Basuh_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
